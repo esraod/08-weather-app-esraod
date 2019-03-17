7d6688c2741db3e895deed1e09f77e8bc89b5973
Esra Oktav Dinler <oktavesra@gmail.com>
2019-03-17 14:46:35 +0100

    :zap::heavy_plus_sign: Added API fetch from OpenWeatherMap's API and imported the API-key from seperate file, used props in Today.jsx to see that the fetch works as it should (TODO: to add sunset,sunrise in a seperate fetch + convert the unix time above the city name)

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
+import { API_KEY } from './api/Key'
 
 import Search from './components/nav/Search'
 import Today from './components/weather-card/Today'
@@ -14,12 +15,83 @@ library.add(faSearch,faFrown,faTint,faWind, faLocationArrow, faSun,faCircle)
 class App extends Component {
 
 
+  state = {
+    city: undefined,
+    country: undefined,
+    temperature: undefined,
+    icon: undefined,
+    description: undefined,
+    humidity: undefined,
+    wind: undefined,
+    sunrise: undefined,
+    sunset: undefined,
+    error: "",
+    time: undefined
+  }
+
+
+  componentDidMount() {  
+    this.getLocation(); 
+  }
+
+
+  getLocation = async (e) =>{
+    this.watchId = navigator.geolocation.getCurrentPosition(
+      position => {
+        this.setState({
+          latitude: position.coords.latitude,
+          longitude: position.coords.longitude,
+          error: null
+        },
+        ()=>{
+          var lat = `${this.state.latitude}`;
+          var lon = `${this.state.longitude}`;
+          let API_WEATHER = `http://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${lon}&units=metric&appid=${API_KEY}`;
+          fetch(API_WEATHER)
+            .then(response => response.json())
+            .then(rsp => {
+                  console.log(rsp);
+                  this.setState({
+                    city: rsp.city.name,
+                    country: rsp.city.country,
+                    temperature: Math.floor(rsp.list["0"].main.temp).toFixed(0),
+                    time: rsp.list["0"].dt,
+                    icon: rsp.list["0"].weather["0"].id,
+                    description: rsp.list["0"].weather["0"].description,
+                    humidity: rsp.list[0].main.humidity,
+                    wind: Math.floor(rsp.list[0].wind.speed).toFixed(0),
+                    error: ""
+                  });
+                })
+                .catch(error => {
+                  console.log(error);
+                });
+              });
+              },
+                error => this.setState({ error: error.message }),
+                {
+                  enableHighAccuracy: true,
+                  timeout: 20000,
+                  maximumAge: 1000,
+                  distanceFilter: 5
+                }
+              );
+            } 
 
   render() {
     return (
       <div>
         <Search />
-        <Today />
+        <Today 
+          city={this.state.city}
+          country={this.state.country}
+          temperature={this.state.temperature} 
+          icon={this.state.icon}
+          description={this.state.description}
+          humidity={this.state.humidity}
+          wind={this.state.wind}
+          time ={this.state.time}
+        />
         <Week />
       </div>
     );
--- src/components/weather-card/Today.jsx
+++ src/components/weather-card/Today.jsx
@@ -7,14 +7,18 @@ const Today = props => (
     <div className="weather-content">
     
         <div className="flex-one">
+            {props.city && props.country && props.time && (
             <div className="weather-item">
-                <p className="weather-value time">1:00pm</p>
-                <p className="weather-value city">Stockholm</p>
-                <p className="weather-value country">SE</p>
+                <p className="weather-value time">{ props.time }</p>
+                <p className="weather-value city">{ props.city }</p>
+                <p className="weather-value country">{ props.country }</p>
             </div>
+            )}
+            {props.temperature && (
             <div className="weather-item">
-            <h2 className="weather-value temp">-3<span className="celsius">°C</span></h2>
+            <h2 className="weather-value temp">{ props.temperature }<span className="celsius">°C</span></h2>
             </div>
+            )}
         </div>
 
         <div className="flex-two">
@@ -23,33 +27,36 @@ const Today = props => (
                 <FontAwesomeIcon className="icon" icon="sun" />{" "}
                 </div>
             </div>
+            {props.description  && ( 
             <div className="weather-item">
-                <p className="weather-value desc">sunny</p>
+                <p className="weather-value desc">{ props.description}</p>
             </div>
+            )}
         </div>
 
         <div className="flex-three">
-
+            
             <div className="weather-item-small">
             <FontAwesomeIcon className="icon-small-desc" icon="sun" />{" "}
             <div className="wind">6am</div>
             </div>
-        
+            {props.wind && (
             <div className="weather-item-small">
             <FontAwesomeIcon className="icon-small-desc" icon="wind" />{" "}
-            <div className="wind">4m/s</div>
+            <div className="wind">{ props.wind }m/s</div>
             </div>
-
+             )}
+            {props.humidity && (
             <div className="weather-item-small">
             <FontAwesomeIcon className="icon-small-desc" icon="tint" />
-            <div className="wind">89% </div>
+            <div className="wind">{ props.humidity }% </div>
             </div>
-
+            )}
             <div className="weather-item-small">
             <FontAwesomeIcon className="icon-small-desc" icon="sun" />
             <div className="wind">6pm</div>
             </div>
-
+          
         </div>
 
 
